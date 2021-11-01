fe8214f92b96ce113dfd4359d2f75154cc5332cf
Manikantasripadi <59179052+Manikantasripadi@users.noreply.github.com>
2021-11-01 11:21:35 +0530

    Create suspend.php

---
+++ suspend.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<style>
+*{box-sizing:border-box}body{font-family:sans-serif;background-image:url(bgimage.jpg);repeat:no-repeat}.txt1{color:#0070bb;font-size:40px;font-weight:800;text-align:center;margin-top:20px}.txt2{color:#c8c6ce;font-size:18px;text-align:center;margin-top:20px}.txt3{color:#c8c6ce;font-size:18px;text-align:center;margin-top:20px}.logo{text-align:center;margin-top:10px}.button{border:2px solid none;padding:10px 40px;background:#0070bb;color:#fff;text-align:center;width:300px;border-radius:25px;margin:20px auto;transition:all .3s ease 0s;-webkit-box-shadow:0 11px 20px -8px #0070bb;-moz-box-shadow:0 11px 20px -8px #0070bb;box-shadow:0 11px 20px -8px #0070bb}.button:hover{border:2px solid none;padding:10px 40px;background:#0070bb;color:#fff;text-align:center;width:300px;border-radius:25px;margin:5px auto;transition:all .3s ease 0s;-webkit-box-shadow:0 19px 34px -8px #0070bb;-moz-box-shadow:0 19px 34px -8px #0070bb;box-shadow:0 19px 34px -8px #0070bb}a .button{color:ffffff;text-align:center;font-size:16px;text-decoration:none}a{text-decoration:none}a:hover{text-decoration:none}#orbit-system{position:relative;width:18em;height:18em;margin:0 auto}.system{position:relative;width:100%;height:100%;-webkit-transform:rotateX(75deg) rotateY(-30deg);transform:rotateX(75deg) rotateY(-30deg);-webkit-transform-style:preserve-3d;transform-style:preserve-3d}.planet,.satellite,.satellite-orbit{position:absolute;top:50%;left:50%;-webkit-transform-style:preserve-3d;transform-style:preserve-3d}.planet{width:9em;height:9em;margin-top:-20em;margin-left:-4.5em;border-radius:50%;background-color:none;color:#fff;text-align:center;line-height:9em;-webkit-transform:rotateY(30deg) rotateX(-75deg);transform:rotateY(30deg) rotateX(-75deg)}.satellite-orbit{width:16em;height:16em;margin-top:-8em;margin-left:-8em;border-radius:50%;-webkit-animation-name:orbit;animation-name:orbit;-webkit-animation-duration:10s;animation-duration:10s;-webkit-animation-iteration-count:infinite;animation-iteration-count:infinite;-webkit-animation-timing-function:linear;animation-timing-function:linear}.satellite{top:100%;width:7em;height:7em;margin-top:-1.5em;margin-left:-1.5em;color:#fefefe;background-color:#0070bb;border-radius:50%;text-align:center;font-weight:600;line-height:7em;-webkit-animation-name:invert-orbit;animation-name:invert-orbit;-webkit-animation-duration:3s;animation-duration:3s;-webkit-animation-iteration-count:infinite;animation-iteration-count:infinite;-webkit-animation-timing-function:linear;animation-timing-function:linear}@-webkit-keyframes orbit{0%{transform:rotateZ(0)}100%{transform:rotateZ(360deg)}}@keyframes orbit{0%{transform:rotateZ(0)}100%{transform:rotateZ(360deg)}}@-webkit-keyframes invert-orbit{0%{transform:rotateZ(360deg) rotateY(30deg) rotateX(-75deg)}100%{transform:rotateZ(0) rotateY(30deg) rotateX(-75deg)}}@keyframes invert-orbit{0%{transform:rotateZ(360deg) rotateY(30deg) rotateX(-75deg)}100%{transform:rotateZ(0) rotateY(30deg) rotateX(-75deg)}}
+</style>
+  </head>
+  <body>
+<div class="txt1">ACCOUNT SUSPENDED</div>
+<div class="txt2">This Domain <?php echo $_SERVER['SERVER_NAME']; ?> has been suspended.</div>
+<div class="logo"><img src="https://raw.githubusercontent.com/hostingaro/wp/main/logos/hostingaro.png" width="200px"></div>
+<div id="orbit-system">
+  <div class="system">
+    <div class="satellite-orbit">
+      <div class="satellite">SUSPENDED</div>
+    </div>
+    <div class="planet"><img src="https://raw.githubusercontent.com/neoistone/neoistone/master/d61wk0d-e73d0189-4605-4fe2-9149-8ed5f5d09129.png" height="200px"> </div>
+  </div>
+</div>
+<div class="txt3">Please Login Account Renew</div>
+<div class="txt3">Suspend date after 15days permanently Delete</div>
+<a href="https://console.hostingaro.com"><div class="button">Login</div></a>
+</body>
+</html>
